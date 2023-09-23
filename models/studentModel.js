d48b70280e3b9f0055e5ade4e819059c95706bb7
TaiAnh124 <20110397@student.hcmute.edu.vn>
2023-09-23 17:27:42 +0700

    week3

---
+++ models/studentModel.js
@@ -0,0 +1,18 @@
+const mygroup = [
+    { id: "19110122", name: "Tran Anh Tai" },
+    { id: "20110397", name: "Pham Quoc Thai" },
+    { id: "20110363", name: "Trinh Van Dong" },
+  ];
+  
+
+module.exports = {
+  getMyGroup: () => mygroup,
+  addItemToMyGroup: (item) => {
+    if (!mygroup.some((existingItem) => existingItem.id === item.id)) {
+      mygroup.push(item);
+      return true;
+    }
+    return false;
+  },
+  findStudentById: (id) => mygroup.find((student) => student.id === id),
+};
